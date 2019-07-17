5309a87c8b53655d8a530e1ad4a764edab95e735
Musa7 <51760137+Musa7@users.noreply.github.com>
2019-07-17 21:57:02 +0200

    updated front.php file

--- src/front.php
+++ src/front.php
@@ -3,53 +3,28 @@ ini_set('display_errors', true);
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\Routing;
+
 
 $request = Request::createFromGlobals();
+
+
 //$ruta = $_SERVER["REQUEST_URI"];
 $ruta = $request->getPathInfo();
 
-switch ($ruta){
-	case "/home":
-		include "view/home.html";
-		break;
-	case "/empresa":
-		include "view/empresa.html";
-		break;
-	case "/biblioteca":
-		include "view/biblioteca.html";
-		break;	
-	case "/subastas":
-		include "view/subastas.html";
-		break;
-	case "/blog":
-		include "view/blog.html";
-		break;	
-	case "/shop":
-		include "view/shop.html";
-		break;
-	case "/signup":
-		include "view/signup.html";
-		break;	
-	case "/login":
-		include "view/login.html";
-		break;		
-	case "/tazas":
-		include "view/tazas.html";
-		break;
-	case "/platos":
-		include "view/platos.html";
-		break;
-	case "/legal":
-		include "view/legal.html";
-		break;
-	case "/cookies":
-		include "view/cookies.html";
-		break;
-	case "/faqs":
-		include "view/faqs.html";
-		break;
-	default:
-		include "view/error.html";
-		break;
+
+
+if ($ruta == '/home'){
+
+	$Response = new BinaryFileResponse(__DIR__.'/view/home.html');
+
+}else{
+	$Response = new BinaryFileResponse(__DIR__.'/view/error.html');
+
 }
+
+$Response->send();
+
 ?>
