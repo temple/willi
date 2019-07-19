fa3c3f66d3f28734268b3db10aae0e868ce85138
Leyre <52932945+1eyre@users.noreply.github.com>
2019-07-19 15:48:54 +0200

    Update front.php

--- src/front.php
+++ src/front.php
@@ -1,51 +1,41 @@
-<?php
+
+
 ini_set('display_errors', true);
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\Routing;
-
-
+//use Symfony\Component\Routing;
+$request = new Request();
+//Se crea un objeto request
 $request = Request::createFromGlobals();
-// $request recoge info del servidor en relación a la superglobal $_REQUEST
-// https://www.php.net/manual/en/reserved.variables.request.php
-
 //$ruta = $_SERVER["REQUEST_URI"];
-$ruta = $request->getPathInfo();
-// Antes obteníamos la ruta así... el componente de enrutamiento podría
-// usar esta ruta
+//Se crea un objeto request usando globals (la informacion del servidor)
 
-// Aquí se determina la vista responsable de atender la petición ($request)
-if ($ruta == '/home'){
+var_dump($request);
+exit;
 
-	$Response = new BinaryFileResponse(__DIR__.'/view/home.html');
+//$ruta = $request ->getPathInfo()
+$url = 'www.williportzellan.com/home';
+include ('router.php');
 
-}else{
-	$Response = new BinaryFileResponse(__DIR__.'/view/error.html');
-
-}
-// el if..else ya es obsoleto según el enunciado.
-// esto ya no lo hace front.php
-
-//TODO: Necesitamos un componente de enrutamiento que determine el controlador
-//      responsable de atender la petición ($request)
-
-//TODO: Este componente de enrutamiento también determina la acción
-//      a invocar en dicho controlador
+$router = null;
+//componente de enrutamiento
+//Todo: crear un componente de enrutamiento
+$back_controller = getController($url);
+//controller delegado
+//Todo: conseguir el valor de $back_controller a partir del $router
+//$action = 'indexAction';
+$action = getAction($url);
 
 
-//TODO: Hay que responder a la petición ($request)
-//      La respuesta se hace delegando en otro controlador:
-//      el controlador responsable de atender la petición ($request)
-//      Necesitamos disponer de este controlador!!
-//      La delegación funciona ejecutando una acción de este controllador
+include($back_controller);
 
 
-$Response->send();
-// esta línea es antiguas, hay que cambiar esto y ponerlo al día
-// el front ya no delega en una vista, sinó que delega en un controllador
-// todo el tema del envio de la respuesta
+$controller = new $class();
+var_dump($controller);
+exit;
 
 ?>
