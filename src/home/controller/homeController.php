1a3fd5898054b15a47ecb42ad0f5353fa41e9f48
Leyre <52932945+1eyre@users.noreply.github.com>
2019-07-19 19:04:14 +0200

    Update homeController.php

--- src/home/controller/homeController.php
+++ src/home/controller/homeController.php
@@ -1,19 +1,11 @@
 <?php
 
-//Es el controlador que responde la petición HTTP con una respuesta en HTML
-
-//Se crea un objeto request
-$request = new Request();
-$request = $request->getPathInfo();
-//En ruta se guarda l
-
-//$Response->send();
 function indexAction($request){
 
-if ($request == '/home'){
-	$request = new BinaryFileResponse(__DIR__.'/principal/view/home.html');
+$response = new BinaryFileResponse(__DIR__.'/../view/home.html');
+$response->send();
+
 }
 
 
-indexAction($request);
 ?>
