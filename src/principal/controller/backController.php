80307c4b99508623b55ed00ccf25edf572254a83
Leyre <52932945+1eyre@users.noreply.github.com>
2019-07-19 18:34:46 +0200

    Update backController.php

--- src/principal/controller/backController.php
+++ src/principal/controller/backController.php
@@ -1,10 +1,22 @@
 <?php
 
-$ruta = $request ->getPathInfo();
+//Es el controlador que responde la petición HTTP con una respuesta en HTML
+
+//Se crea un objeto request
+$request = new Request();
+$ruta = $request->getPathInfo();
+//En ruta se guarda l
+
+//$Response->send();
+function indexAction($ruta){
 
 if ($ruta == '/home'){
-	$Response = new BinaryFileResponse(__DIR__.'/view/home.html');
+	$Response = new BinaryFileResponse(__DIR__.'/principal/view/home.html');
 }else{
-	$Response = new BinaryFileResponse(__DIR__.'/view/error.html');}
-$Response->send();
+	$Response = new BinaryFileResponse(__DIR__.'/principal/view/error.html');
+}
+
+}
+
+indexAction($Response);
 ?>
