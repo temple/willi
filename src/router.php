9f2c3b2fce27bce4b418a1f5533a3853e2925c90
Leyre <52932945+1eyre@users.noreply.github.com>
2019-07-19 19:58:31 +0200

    Update router.php

--- src/router.php
+++ src/router.php
@@ -1,13 +1,23 @@
 <?php
-class routing{
 
+class router{
+//Creamos la clase router
 function getController(string $url){
-	return 'controller/backController.php';
+//La función getController recibe como parámetro un string
+
+if ($ruta == '/home'){
+	return 'controller/homeController.php';
+	//Si el contenido del parámetro es igual a "/home", devuelve la ruta donde está homeController.
+}else{
+	return 'controller/errorController.php';
+		//Si el contenido del parámetro es diferente a "/home", devuelve la ruta donde está errorController.
 }
 
 function getAction (string $url){
 	return 'indexAction';
 }
+//La función getAction recibe como parámetro un string llamado url y devuelve un string.
 }
 
 ?>
+
