6cda6ba99df79f18e2e8a0845fad35c498e29a3b
Leyre <52932945+1eyre@users.noreply.github.com>
2019-07-19 20:30:35 +0200

    Update router.php

--- src/router.php
+++ src/router.php
@@ -6,10 +6,10 @@ function getController(string $url){
 //La función getController recibe como parámetro un string
 
 if ($ruta == '/home'){
-	return 'controller/homeController.php';
+	return 'home/controller/homeController.php';
 	//Si el contenido del parámetro es igual a "/home", devuelve la ruta donde está homeController.
 }else{
-	return 'controller/errorController.php';
+	return 'error/controller/errorController.php';
 		//Si el contenido del parámetro es diferente a "/home", devuelve la ruta donde está errorController.
 }
 
