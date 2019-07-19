7ef4bb8d126dc8c55b5e99eedebe213f50a00ab4
Xavier Subirà <xavier-subira@yandex.com>
2019-07-19 15:35:21 +0200

    actualizado front con mensajes explicativos de lo que hay que hacer en la práctica

--- src/front.php
+++ src/front.php
@@ -9,13 +9,15 @@ use Symfony\Component\Routing;
 
 
 $request = Request::createFromGlobals();
-
+// $request recoge info del servidor en relación a la superglobal $_REQUEST
+// https://www.php.net/manual/en/reserved.variables.request.php
 
 //$ruta = $_SERVER["REQUEST_URI"];
 $ruta = $request->getPathInfo();
+// Antes obteníamos la ruta así... el componente de enrutamiento podría
+// usar esta ruta
 
-
-
+// Aquí se determina la vista responsable de atender la petición ($request)
 if ($ruta == '/home'){
 
 	$Response = new BinaryFileResponse(__DIR__.'/view/home.html');
@@ -24,7 +26,26 @@ if ($ruta == '/home'){
 	$Response = new BinaryFileResponse(__DIR__.'/view/error.html');
 
 }
+// el if..else ya es obsoleto según el enunciado.
+// esto ya no lo hace front.php
+
+//TODO: Necesitamos un componente de enrutamiento que determine el controlador
+//      responsable de atender la petición ($request)
+
+//TODO: Este componente de enrutamiento también determina la acción
+//      a invocar en dicho controlador
+
+
+//TODO: Hay que responder a la petición ($request)
+//      La respuesta se hace delegando en otro controlador:
+//      el controlador responsable de atender la petición ($request)
+//      Necesitamos disponer de este controlador!!
+//      La delegación funciona ejecutando una acción de este controllador
+
 
 $Response->send();
+// esta línea es antiguas, hay que cambiar esto y ponerlo al día
+// el front ya no delega en una vista, sinó que delega en un controllador
+// todo el tema del envio de la respuesta
 
 ?>
